807e796152979204a1aa1742f415691025b66179
juan manuel sanchez <116232004+juan1214s@users.noreply.github.com>
2025-06-28 22:18:26 -0500

    Cambie dos animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { ProfileComponent } from './component/profile/profile.component';
@@ -9,10 +11,27 @@ import { TechIconComponent } from './component/tech-icon/tech-icon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, HeaderComponent, ProfileComponent, ExperienceComponent, ProyectsComponent, EstudiesComponent, TechIconComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProfileComponent,
+    ExperienceComponent,
+    ProyectsComponent,
+    EstudiesComponent,
+    TechIconComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Juan Manuel Sanchez';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // 👈 Esto asegura scroll al top
+      });
+  }
 }
